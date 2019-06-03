891fdee90f7e6e68ad951ba7f5eca43ce93ec8ab
KUPASH <alexshapuk@yandex.ru>
2019-06-03 21:09:02 +0300

    forth commit

--- update.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$update_string = $_GET['id'];
-$file = file('tasks.txt');
-str_replace($_GET['update'], $_GET['task'], $update_string );
-file_put_contents('tasks.txt', implode("", $file) );
-header('Location: index.php');
