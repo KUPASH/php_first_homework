6962fb11d80401b3dab8e3301c7516688acd8786
KUPASH <alexshapuk@yandex.ru>
2019-06-03 21:02:47 +0300

    third commit

--- index.php
+++ index.php
@@ -15,7 +15,9 @@
 <body class="<?=$class?>">
     <form>
         New task: <input name="task">
-        <button type="submit">Create</button>
+        <button type="submit">Create</button> </br>
+        <input name="update">
+        <button type="submit">Update</button>
     </form>
     <?
     if ($_GET['task']) {
@@ -26,16 +28,21 @@
     $handle = fopen('tasks.txt', 'r');
     echo '<table border="1">';
     $counter = 0;
-    $counter1 = 0;
     while(($b = fgets($handle)) !== false) {
         echo '<tr><td>'.$b.'</td>
-                  <td><a href="redirect.php?id='.$counter++.'">X</a></td>
-                  <td><input name="update"></td>
-                  <td><a href="update.php?id='.$counter1++.'">Update</a></td></tr>';
-
+                  <td><a href="redirect.php?id='.$counter++.'">X</a></td></tr>';
     }
     echo '</table>';
     fclose($handle);
+
+    if ($_GET['update']) {
+        $handle = fopen('tasks.txt', 'w');
+        fputs($handle, $_GET['update'] . PHP_EOL);
+        fclose($handle);
+    }
+
+
+
 
 
 
